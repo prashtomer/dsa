e168d2c2fd28d25a789ca038ec20a318bdfb7391
prashtomer <prash.tomer@gmail.com>
2022-12-22 14:25:40 +0530

    restructuring based on algorithm type

--- palindrome/index.js
+++ palindrome/index.js
@@ -23,3 +23,12 @@ module.exports = palindrome;
 //
 //   return str === reversed;
 // }
+
+// function palindrome(str) {
+//   for(let i = 0; i < Math.floor(str.length / 2); i++) {
+//     if(!(str[i] === str[str.length - i - 1])) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
