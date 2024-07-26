f72b8c2eccb75bf21b45bfacaf2c14ebfe747ae5
prashtomer <prash.tomer@gmail.com>
2024-07-26 15:54:20 +0530

    Add more algos

---
+++ isValidParantheses.js
@@ -0,0 +1,16 @@
+function isValidParentheses(s) {
+  const stack = [];
+  const pairs = { '(': ')', '{': '}', '[': ']' };
+  for (const char of s) {
+    if (char in pairs) {
+      stack.push(char);
+    } else {
+      if (pairs[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log(isValidParentheses("{[()]}"));
